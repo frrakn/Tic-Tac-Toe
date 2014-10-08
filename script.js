7d7a48406442f82a1d9d80883c72628b8b8d20d5
frrakn <cheng.frank.chen@gmail.com>
2014-10-08 15:22:03 -0400

    10/8

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 $(document).ready(function()
 {
-	$(".clickbox").click(function()
+    //creates array to match display
+    var rawBoxes = $(".clickbox").get();
+    var boxes = [];
+    for(var i = 0; i < 3; i++)
+    {
+        if(!boxes[i])
+            boxes[i] = [];
+        for(var j = 0; j < 3; j++)
+        {
+            boxes[i][j] = rawBoxes[i*3+j];
+        }
+    }
+    
+    //specifies turn, even = X, odd = O
+    var turn = 0;
+    
+    //defines winner
+    var checkWinner = function checkWinner()
+    {
+        
+    };
+    
+	$(".clickbox").hover(function()
 	{
-		$(this).addClass("clickbox-click");
-	});
+	    if($(this).hasClass("clickbox"))
+		    $(this).addClass("clickbox-hover");
+	},
+	function()
+	{
+        $(this).removeClass("clickbox-hover");
+    });
+    
+    $(".clickbox").click(function()
+    {
+        $(this).addClass("clickbox-clicked");
+        $(this).removeClass("clickbox");
+    });
 });
